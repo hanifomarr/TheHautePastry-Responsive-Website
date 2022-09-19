405cb73dc0f9716b291ab57e1c60da81d852986b
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2022-09-19 18:23:20 +0800

    FAQs toggle js

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+// change Navbar scroll
+
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
+})
+
+// show/hide FAQs
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon span');
+            if(icon.innerHTML === 'add') {
+                icon.innerHTML = "remove";
+            } else{
+                icon.innerHTML = 'add';
+            }
+    })
 })
